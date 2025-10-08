ce38f470ea7bf8f3d22980194bbe8436c91a058f
hyacinthe <hspanjerspro@gmail.com>
2025-10-08 14:48:46 +0200

    changer port

--- server/socketServer.js
+++ server/socketServer.js
@@ -318,7 +318,7 @@ io.on("connection", (socket) => {
 
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;;
 server.listen(PORT, () => console.log(`✅ Socket server running on port ${PORT}`));
 
 
